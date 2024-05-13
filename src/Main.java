8da90e55ddc940792b0d51f5aa2bae39963d4b46
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-05-13 09:05:16 +0200

    Klar(tror jag)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int portnumber = 8880;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+        try{
+            MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
+
+            InetAddress group = InetAddress.getByName("224.4.5.6");
+
+            chatMulticastSocket.joinGroup(group);
+
+            String msg = "";
+            System.out.println("Type a message for the server: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+            chatMulticastSocket.send(data);
+
+            chatMulticastSocket.close();
+        }catch (IOException ie){
+            ie.printStackTrace();
+        }
     }
 }
